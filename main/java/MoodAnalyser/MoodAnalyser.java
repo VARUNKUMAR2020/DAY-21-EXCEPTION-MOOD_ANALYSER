9de250a418417d74fb5894a4843542a802f82af7
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-10-02 11:00:29 +0530

    EXCEPTION HANDLING

---
+++ main/java/MoodAnalyser/MoodAnalyser.java
@@ -0,0 +1,13 @@
+package MoodAnalyser;
+
+/**
+ * Hello world!
+ *
+ */
+public class MoodAnalyser 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ test/java/MoodAnalyser/MoodAnalyserTest.java
@@ -0,0 +1,13 @@
+package MoodAnalyser;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
