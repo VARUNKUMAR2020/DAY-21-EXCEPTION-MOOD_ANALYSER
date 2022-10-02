0ca1a1dcff4c230f80fcc8a9d3abd8567ab0f5f5
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-10-02 12:35:26 +0530

    ADD: REPEATING THE FIRST TEST CASE

--- test/java/MoodAnalyser/MoodAnalyserTest.java
+++ test/java/MoodAnalyser/MoodAnalyserTest.java
@@ -1,22 +1,15 @@
 package MoodAnalyser;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-	MoodAnalyser newMood = new MoodAnalyser();
+	
 
 	@Test
 	public void checkTheMood1() {
-		String mood = newMood.analyseMood("I am sad");
-		Assert.assertEquals("SAD",mood);
-	}
-
-	@Test
-	public void checkTheMood2() {
-		String mood = newMood.analyseMood("I am sad");
-		Assert.assertEquals("HAPPY",mood);
+		MoodAnalyser newMood = new MoodAnalyser();
+		String actualResult = MoodAnalyser.checkmood("I am in sad Mood");
+		Assert.assertEquals("SAD",actualResult);
 	}
 }
