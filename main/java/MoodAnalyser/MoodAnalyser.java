1890d615442b28271764d8fa7d3c205930705a70
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-10-02 11:05:23 +0530

    ADD: TO ANALYSE AND RESPOND HAPPY OR SAD

--- main/java/MoodAnalyser/MoodAnalyser.java
+++ main/java/MoodAnalyser/MoodAnalyser.java
@@ -1,13 +1,22 @@
 package MoodAnalyser;
 
-/**
- * Hello world!
- *
- */
-public class MoodAnalyser 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.Scanner;
+
+public class MoodAnalyser {
+	static String analyseMood(String checkmood) {
+		if (checkmood.toLowerCase().contains("happy")) {
+			System.out.println("HAPPY MOOD");
+		} else {
+			System.out.println("SAD MOOD");
+		}
+		return checkmood;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("EXCEPTION HANDLING");
+		Scanner analyse = new Scanner(System.in);
+		System.out.println("ENTER THE MOOD YOUR:  ");
+		String Mood = analyse.nextLine();
+		analyseMood(Mood);
+	}
 }
