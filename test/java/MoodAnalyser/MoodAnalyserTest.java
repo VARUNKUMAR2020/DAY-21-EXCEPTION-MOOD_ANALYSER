2a9f0cd09c7fc2b208c4572c959c5983a2a19f02
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-10-02 11:25:46 +0530

    ADD: TEST CASE-ANY MOOD RETURN HAPPY

--- test/java/MoodAnalyser/MoodAnalyserTest.java
+++ test/java/MoodAnalyser/MoodAnalyserTest.java
@@ -9,8 +9,14 @@ public class MoodAnalyserTest {
 	MoodAnalyser newMood = new MoodAnalyser();
 
 	@Test
-	public void checkTheMood() {
+	public void checkTheMood1() {
 		String mood = newMood.analyseMood("I am sad");
-		Assert.assertTrue(mood,true);
+		Assert.assertEquals("SAD",mood);
+	}
+
+	@Test
+	public void checkTheMood2() {
+		String mood = newMood.analyseMood("I am sad");
+		Assert.assertEquals("HAPPY",mood);
 	}
 }
